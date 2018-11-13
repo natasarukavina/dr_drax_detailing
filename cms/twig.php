0ee797a0b0944eb399da02513ae12bdf49cc23ab
igor <igor@IgorBuildconMBP.local>
2018-11-13 16:11:00 +0100

    _BASE var for twig /

--- cms/twig.php
+++ cms/twig.php
@@ -15,7 +15,7 @@ function getBaseUrl()
     // output: http://
     $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https://'?'https://':'http://';
     // return: http://localhost/myproject/
-    return $protocol.$hostName. dirname($pathInfo['dirname']);
+    return $protocol.$hostName. dirname($pathInfo['dirname']).'/';
 }
 //$templates_assoc = fetch_twig_templates();
 //print_r($templates_assoc);
